609f3869d9e0333f4c7e657c5b007c98af2f4a08
EffaOjah <effaojah@gmail.com>
2026-07-11 18:26:19 +0100

    Initial coming soon frontend

---
+++ src/pages/ComingSoonPage.tsx
@@ -0,0 +1,8 @@
+export default function ComingSoonPage() {
+  return (
+    <main className="coming-soon">
+      <img src="/img/GH1.png" alt="GadgetHub" className="coming-soon__logo" />
+      <h1>New Frontend Coming soon</h1>
+    </main>
+  );
+}
